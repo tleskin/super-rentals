7c90fc865db4cc0e912a7613218b44e93db95000
Tom Leskin <tleskin@gmail.com>
2016-10-31 22:42:28 -0400

    Initial commit

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,26 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | list rentals');
+
+// test('should redirect to rentals route', function (assert) {
+//   visit('/');
+//   andThen(function(){
+//     assert.qual(currentURL(), '/rentals', 'should redirect automatically');
+//   })
+// });
+//
+// test('should list available rentals.', function (assert) {
+// });
+//
+// test('should link to information about the company.', function (assert) {
+// });
+//
+// test('should link to contact information.', function (assert) {
+// });
+//
+// test('should filter the list of rentals by city.', function (assert) {
+// });
+//
+// test('should show details for a specific rental', function (assert) {
+// });
